bbb45849b876d08c7732b1b9117287b8867cfc73
Garry Hall <garrymhall@gmail.com>
2021-05-20 12:48:11 +0100

    first commit - hello world template

---
+++ models/event.py
@@ -0,0 +1,8 @@
+class Event:
+    def __init__(self, date, name, no_of_guests, location, description):
+        self.date = date
+        self.name = name
+        self.no_of_guests = no_of_guests
+        self.location = location
+        self.description = description
+        
---
+++ models/event_functions.py
@@ -0,0 +1,7 @@
+from models.event import Event
+import datetime
+
+event_1 = Event(datetime.datetime(2021, 5, 17), "Barbecue", 100, "Steve's House", "Let's all ome round for a barbecue")
+events = [event_1]
+
+
