0fb74a95a72c71321640e6309141a7414a586a07
Garry Hall <garrymhall@gmail.com>
2021-05-20 13:06:47 +0100

    display events on home page

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,11 @@
 from app import app
 from flask import render_template
+from models.event_list import events
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', events=events)
+
+@app.route('/new-event')
+def add_event():
+    return render_template('new-event.html')
