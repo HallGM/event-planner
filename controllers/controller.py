ece6b9f014843cb6b1736cd2a2377cb6c4829f19
Garry Hall <garrymhall@gmail.com>
2021-05-23 09:39:20 +0100

    add new task functionality

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,8 @@
+from threading import Event
 from app import app
-from flask import render_template
+from flask import render_template, request
 from models.event_list import events
+from models.event import Event
 
 @app.route('/')
 def index():
@@ -9,3 +11,11 @@ def index():
 @app.route('/new-event')
 def add_event():
     return render_template('new-event.html')
+
+@app.route('/new-event', methods=['POST'])
+def new_event():
+    form = request.form
+    print(type(form['event-date']))
+    new_event = Event(form['event-date'], form['event-name'], int(form['guests']), form['location'], form['description'])
+    events.append(new_event)
+    return render_template('index.html', events=events)
